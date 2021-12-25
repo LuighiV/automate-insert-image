18676e725f62862f75abecc539147eb810667f00
Luighi Viton <luighiavz@gmail.com>
2021-12-25 13:36:23 -0500

    Agregar archivos de proyecto.

---
+++ editdocuments/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+using CommandLine;
+
+namespace editdocuments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start program");
+            string FilePath = @"D:\Luighi\Documents\SITUACIÓN FINAL 3 años INFORME DEL PROGRESO DEL-LA ESTUDIANTE 2021.doc";
+            string PicturePath = @"D:\Luighi\Documents\firmaLuighiViton.png";
+            var WordApp = new Word.Application();
+
+            RunOnFile(FilePath, PicturePath, "DIRECTORA (e)", 280, 10, WordApp, false, null, false);
+
+            WordApp.Quit();
+
+            Console.WriteLine("Finish program");
+        }
+
+        static void RunOnFile(string FilePath, 
+            string PicturePath,
+            string TextPlaceHolder,
+            int LeftOffset,
+            int BottomOffset,
+            Word.Application WordApp=null,
+            bool WordAppVisible=true,
+            string FolderSave=null,
+            bool SaveFile=false)
+        {
+            var WordDocument = new WordDocument(FilePath, WordAppVisible, WordApp);
+
+            WordDocument.GetRange(TextPlaceHolder);
+            WordDocument.addPicture(PicturePath, LeftOffset, BottomOffset);
+            WordDocument.SaveAsPDF(FolderSave);
+            Console.ReadKey();
+            WordDocument.Close(SaveFile);
+        }
+    }
+}
---
+++ editdocuments/WordDocument.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+using System.IO;
+
+namespace editdocuments
+{
+    internal class WordDocument
+    {
+        public string DocumentPath { get; set; }
+
+        public Word.Application WordApp;
+        public Word.Document Document = new Word.Document() { };
+        
+        public Word.Range Range { get; set; }
+
+        public WordDocument(string Path, bool visible = false, Word.Application WordApp = null)
+        {
+            this.DocumentPath = Path;
+            if (WordApp == null)
+            {
+                WordApp = new Word.Application() { };
+            }
+            this.WordApp = WordApp;
+            this.WordApp.Visible = visible;
+            this.Document = this.WordApp.Documents.Open(Path);
+
+        }
+
+        internal void SaveAsPDF(string FolderName=null)
+        {
+            string Name = this.Document.Name;
+            string NewName = Path.ChangeExtension(Name, "pdf");
+
+            if (FolderName != null)
+                NewName = Path.Combine(FolderName, NewName);
+
+            Console.WriteLine("Current filename: {0}", Name);
+            this.Document.SaveAs2(FileName: NewName, FileFormat: Word.WdSaveFormat.wdFormatPDF);
+        }
+
+        internal void Close(bool save=true)
+        {
+            if (save)
+                this.Document.Close(Word.WdSaveOptions.wdSaveChanges);
+            else
+                this.Document.Close(Word.WdSaveOptions.wdDoNotSaveChanges);
+        }
+
+        internal void addPicture(string picture, int LeftOffset = 0, int BottomOffset = 0, int Width = 200,
+             int Height=150)
+        {
+            //var borders = this.Range.Borders;
+            //Console.WriteLine(borders.DistanceFromLeft);
+            var shape = this.Document.Shapes.AddPicture(picture,
+                Left: LeftOffset,
+                Top: BottomOffset - Height,
+                Width: Width,
+                Height: Height,
+                Anchor: this.Range);
+            
+        }
+
+        public Word.Range GetRange(string text)
+        {
+            var range = this.Document.Content;
+            range.Find.Execute(text);
+            this.Range = range;
+            Console.WriteLine(this.Range.Text);
+            return range;
+        }
+    }
+}
